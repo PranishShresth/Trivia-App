9156bbdcee2e163d2e71b74920aaaf55c3257253
Pranish Shrestha <shresthapranish99@gmail.com>
2020-07-21 17:32:50 +1000

    Updates

---
+++ precache-manifest.079ee5c15e37e8ac35014c1163832767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27fadec86b6c6759260d53029b07859",
+    "url": "/Trivia-App/index.html"
+  },
+  {
+    "revision": "ebdf3cac449039804d3e",
+    "url": "/Trivia-App/static/css/main.bc217de8.chunk.css"
+  },
+  {
+    "revision": "40bee047a41a1f02a7bb",
+    "url": "/Trivia-App/static/js/2.c90f8b8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Trivia-App/static/js/2.c90f8b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdf3cac449039804d3e",
+    "url": "/Trivia-App/static/js/main.c0ea43bd.chunk.js"
+  },
+  {
+    "revision": "ddd15ad782adc1a4b7fc",
+    "url": "/Trivia-App/static/js/runtime-main.36b22610.js"
+  }
+]);
